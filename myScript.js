b0e62341f07416688d891dad01f542e6a3ac4316
Aniket Patra <43411221+aniket-patra@users.noreply.github.com>
2019-03-03 17:37:43 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,3 +1,13 @@
 function msg() {
   alert("This page isn't ready yet!!");
 }
+
+function msgTweet()
+{
+  alert("This is fun. Actually, I don't have tweeter account but it looks beautiful on my site! Forgive me?");
+}
+
+function msgGplus()
+{
+  alert("This is fun. Actually, I don't have Google Plus account but it looks beautiful on my site! Forgive me?");
+}
