abf67d0153e887154769e3fd4efa073cf1bd3ab0
Aniket Patra <43411221+aniket-patra@users.noreply.github.com>
2019-03-10 20:54:41 +0530

    Add files via upload

--- myScript.js
+++ myScript.js
@@ -1,5 +1,24 @@
 function msg() {
-  alert("This page isn't really ready yet!! Have fun.");
+  var x = "User-agent header sent: " + navigator.userAgent;
+  var a = x.search(/windows/i);
+  var b = x.search(/android/i);
+  var c = x.search(/macintosh/i);
+  var d = x.search(/iphone/i);
+  var e = x.search(/ipad/i);
+  if(a>-1)
+  alert("This page isn't completely ready yet!! But have fun on your Windows Device.");
+
+  if(b>-1)
+  alert("This page isn't ready for your Android device. But do check it out on PC");
+
+  if(c>-1)
+  alert("This page isn't completely ready yet!! But have fun on your Apple Device.");
+
+  if(d>-1)
+  alert("This page isn't ready for your iPhone device. But do check it out on PC/MAC");
+
+  if(e>-1)
+  alert("I don't know if this page will work on your iPad but have fun on PC/MAC");
 }
 
 function msgTweet()
