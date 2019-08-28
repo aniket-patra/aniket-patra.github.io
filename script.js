1924c9b0187376425c14ff02e7f3cc0bcd77c23e
Aniket Patra <43411221+aniket-patra@users.noreply.github.com>
2019-08-28 11:22:18 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-function scrollFunction() {
-    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-      document.getElementById("header").style.fontSize = "30px";
-    } else {
-      document.getElementById("header").style.fontSize = "90px";
-    }
-  }
 
-  function onSubmit(token) {
-
-    document.getElementById("invisible-recaptcha-form").submit();
-    
-    }
