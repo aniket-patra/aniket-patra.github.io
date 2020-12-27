82f365bbce53113af86f9eb9dc442b4f84e7e7e1
aniket <aniket.howrah@gmail.com>
2020-12-27 10:04:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ function nameCopy()
 {
     var name=document.getElementById("form_name").value;
     document.getElementById("nameFiller").value=name;
-    document.getElementById("subjectSetter").value="Message from "+name+" from Github";
+    document.getElementById("subjectSetter").value="Message from Github profile.";
     localStorage.setItem('userName',name);    
 }
