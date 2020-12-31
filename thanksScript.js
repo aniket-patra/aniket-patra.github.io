dede47f22e1e4fa1b8aa214dd510b3236d1a1dde
aniket <aniket.howrah@gmail.com>
2020-12-31 12:15:02 +0530

    Added auto redirect to thanks.html

--- thanksScript.js
+++ thanksScript.js
@@ -5,3 +5,13 @@ if (nameVar==null)
 }
 document.getElementById('headEdit').innerHTML="Thank You "+nameVar;
 localStorage.clear();
+
+var i = 8;
+      setInterval(function () {
+        document.getElementById("counter").innerHTML = i;
+        i = i - 1;
+
+        if (i == -1) {
+          window.location.href = "https://aniket-patra.github.io/";
+        }
+      }, 1000);
